3080d78eaed5b74f90707d200f16367727c2c3c1
Farooq <farooq1.khan@ryerson.ca>
2023-01-03 15:55:16 -0500

    created figure 1, figure 2-1

--- ch2/fig2_2.py
+++ ch2/fig2_2.py
@@ -1,39 +1,64 @@
 import matplotlib.pyplot as plt
 import numpy as np 
+from tqdm import tqdm, trange 
+import random 
+
 seed = 42
 k = 10
 time_step = 1000
-
+amount_of_games = 2000
 np.random.seed(seed)
-q_star = np.random.normal(size=k) 
-rewards_dist = [] 
-for q in q_star: 
-    rewards_dist.append(np.random.normal(loc=q, size=1000))
-rewards_dist = np.vstack(rewards_dist) 
+actions = [i for i in range(k)]
 
-Q = [0 for _ in range(k)]
-N = [0 for _ in range(k)]
+def bandit(q_star, action): 
+    mean = q_star[action]
+    return np.random.normal(loc=mean)
 
-import random 
-actions = [i for i in range(k)] 
 
-def run(time_step, epsilon=0):
-    print(f'{epsilon=}')
-    total_reward = 0 
-    for _ in range(time_step): 
-        a = np.argmax(Q) 
-        if random.random() < epsilon:
-            a = np.random.choice(actions) 
-        #print(f'selected action: {a=}', end=' ') 
-        r = rewards_dist[a][0] 
-        #print(f'and got reward: {r=}')
-        N[a] += 1
-        x = Q[a] + (1/N[a]) * (r - Q[a])
-        Q[a] = x  
-        total_reward += r 
-    print(f'{Q=}')
-    print(f'{total_reward=:.2f}, average_reward={total_reward/time_step:.2f}')
-print(f'{q_star=}') 
-run(time_step)
-run(time_step, 0.1)
-run(time_step, 0.01)
+def game(epsilon=0):
+    reward_experiments = [] 
+    action_experiments = [] 
+    for i in trange(2000): 
+        q_star = np.random.normal(size=k) 
+        # init Q, N 
+        Q = [0 for _ in range(k)] 
+        N = [0 for _ in  range(k)] 
+        
+        action_history = [-1 for _ in range(1000)] 
+        reward_history = [0 for _ in range(1000)] 
+
+        for j in range(1000): 
+            a = np.argmax(Q) 
+            if random.random() < epsilon:
+                a = np.random.choice(actions) 
+            r = bandit(q_star, a) 
+
+            N[a] += 1
+            x = Q[a] + (1/N[a]) * (r - Q[a])
+            Q[a] = x  
+
+            reward_history[j] = r 
+            action_history[j] = a
+
+        reward_experiments.append(reward_history)
+        action_experiments.append(action_history)
+
+    # plot average reward over 1k run for 2k experiments 
+    reward_experiments_arr = np.array(reward_experiments)
+    return reward_experiments_arr
+
+if __name__ == '__main__': 
+    print(f'Greedy Method')
+    reward_exps_greedy = game(0) 
+    print(f'Epsilon 0.1')
+    reward_exps_ep1 = game(0.1) 
+    print(f'Epsilon 0.01')
+    reward_exps_ep01 = game(0.01) 
+
+    plt.plot([i for i in range(1, 1000+1)], reward_exps_greedy.mean(axis=0), label=f'greedy')
+    plt.plot([i for i in range(1, 1000+1)], reward_exps_ep1.mean(axis=0), label=f'epsilon=0.1')
+    plt.plot([i for i in range(1, 1000+1)], reward_exps_ep01.mean(axis=0), label=f'epsilon=0.01')
+    plt.legend()
+    plt.show()
+
+
